467a59153080f0a55edae63677bbd287d3e9b699
hutabert <hutabert@gmail.com>
2015-05-14 17:42:52 +0200

    Before nb.

--- aesdev.c
+++ aesdev.c
@@ -6,10 +6,19 @@
 #include <linux/slab.h>
 #include <linux/pci.h>
 #include <linux/kernel.h>
+#include <asm/uaccess.h>
+#include <linux/sched.h>
 
 MODULE_LICENSE("GPL");
 
 static int major;
+static int dev_count;
+static aes128_dev *aes_devs[256];
+
+static DECLARE_WAIT_QUEUE_HEAD(big_queue);
+static DEFINE_MUTEX(big_mutex);
+
+struct class *dev_class;
 
 static struct file_operations aes_fops =
 {
@@ -43,16 +52,107 @@ static void init_context(aes128_context *context)
 {
   memset(context, 0, sizeof(aes128_context));
   context->mode = AES_UNDEF;
+  context->read_buffer.buf = kmalloc(AESDRV_IOBUFF_SIZE, GFP_KERNEL);
+  context->write_buffer.buf = kmalloc(AESDRV_IOBUFF_SIZE, GFP_KERNEL);
+  mutex_init(&context->mutex);
+}
+
+static void destroy_context(aes128_context *context)
+{
+  kfree(context->read_buffer.buf);
+  kfree(context->write_buffer.buf);
+  mutex_destroy(&context->mutex);
 }
 
 /*** File handlers ***********************************************************/
 static ssize_t aes_file_read(struct file *f, char __user *buf, size_t len, loff_t *off)
 {
+  aes128_context *context;
+  struct circ_buf *read_buff;
+  size_t in_buffer;
+
+  context = f->private_data;
+  read_buff = &context->read_buffer;
+
+  while (CIRC_CNT(read_buff->head, read_buff->tail, AESDRV_IOBUFF_SIZE) == 0)
+  {
+    wait_event(big_queue, CIRC_CNT(read_buff->head, read_buff->tail, AESDRV_IOBUFF_SIZE) == 0);
+  }
+
+  in_buffer = CIRC_CNT(read_buff->head, read_buff->tail, AESDRV_IOBUFF_SIZE);
+
+  if (in_buffer)
+  {
+    size_t to_read;
+    size_t to_read_1;       /* Bytes to read until end of buffer */
+    size_t to_read_2;       /* Bytes to read from beginning of buffer */
+
+    to_read = min(len, in_buffer);
+    to_read_1 = min((int) to_read, CIRC_SPACE_TO_END(read_buff->head, read_buff->tail, AESDRV_IOBUFF_SIZE));
+    to_read_2 = to_read - to_read_1;
+
+    if (copy_to_user(buf, read_buff->buf + read_buff->head, to_read_1))
+    {
+      printk(KERN_WARNING "copy_to_user\n");
+      return -ENOMEM;
+    }
+    if (to_read_2)
+      if (copy_to_user(buf + to_read_1, read_buff->buf, to_read_2))
+      {
+        printk(KERN_WARNING "copy_to_user\n");
+        return -ENOMEM;
+      }
+
+    read_buff->head += to_read_1;
+    if (to_read >= to_read_2)
+      read_buff->head = to_read_2;
+
+    return to_read;
+  }
+
   return 0;
 }
 
 static ssize_t aes_file_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
 {
+  aes128_context *context;
+  struct circ_buf *write_buff;
+  size_t in_buffer;
+  size_t to_write;
+  size_t to_write_1;
+  size_t to_write_2;
+
+  context = f->private_data;
+  write_buff = &context->write_buffer;
+
+  while (CIRC_SPACE(write_buff->head, write_buff->tail, AESDRV_IOBUFF_SIZE) == 0)
+  {
+    wait_event(big_queue, CIRC_SPACE(write_buff->head, write_buff->tail, AESDRV_IOBUFF_SIZE) == 0);
+  }
+
+  in_buffer = CIRC_SPACE(write_buff->head, write_buff->tail, AESDRV_IOBUFF_SIZE);
+  to_write = min(len, in_buffer);
+  to_write_1 = min((int) to_write, CIRC_CNT_TO_END(write_buff->head, write_buff->tail, AESDRV_IOBUFF_SIZE));
+  to_write_2 = to_write - to_write_1;
+
+  if (copy_from_user(write_buff->buf + write_buff->head, buf, to_write_1))
+  {
+    printk(KERN_WARNING "copy_to_user\n");
+    return -EFAULT;
+  }
+
+  if (to_write_2)
+    if (copy_to_user(write_buff->buf, buf, to_write_2))
+    {
+      printk(KERN_WARNING "copy_to_user\n");
+      return -EFAULT;
+    }
+
+  write_buff->head += to_write_1;
+  /* TODO to jest bug */
+  if (to_write >= to_write_1)
+    write_buff->head = to_write_2;
+
   return len;
 }
 
@@ -60,33 +160,166 @@ static int aes_file_open(struct inode *i, struct file *f)
 {
   aes128_context *context = kmalloc(sizeof(aes128_context), GFP_KERNEL);
   init_context(context);
+
+  context->aes_dev = aes_devs[iminor(i)];
   f->private_data = context;
+  printk(KERN_WARNING "Assigned opened file to device %p\n", context->aes_dev);
   return 0;
 }
 
 static int aes_file_release(struct inode *i, struct file *f)
 {
+  destroy_context(f->private_data);
   kfree(f->private_data);
   return 0;
 }
 
 static long aes_file_ioctl(struct file *f, unsigned int cmd, unsigned long arg)
 {
+  aes128_context *context;
+
+  context = f->private_data;
+
+  if (context->mode == AES_UNDEF && cmd == AESDEV_IOCTL_GET_STATE)
+    return -EINVAL;
+
+  if (cmd == AESDEV_IOCTL_SET_ECB_ENCRYPT)
+  {
+    context->mode = AES_ECB_ENCRYPT;
+  }
+  else if (cmd == AESDEV_IOCTL_SET_ECB_DECRYPT)
+  {
+    context->mode = AES_ECB_DECRYPT;
+  }
+  else if (cmd == AESDEV_IOCTL_SET_CBC_ENCRYPT)
+  {
+    context->mode = AES_CBC_ENCRYPT;
+  }
+  else if (cmd == AESDEV_IOCTL_SET_CBC_DECRYPT)
+  {
+    context->mode = AES_CBC_DECRYPT;
+  }
+  else if (cmd == AESDEV_IOCTL_SET_CFB_ENCRYPT)
+  {
+    context->mode = AES_CBC_DECRYPT;
+  }
+  else if (cmd == AESDEV_IOCTL_SET_CFB_DECRYPT)
+  {
+    context->mode = AES_CBC_DECRYPT;
+  }
+  else if (cmd == AESDEV_IOCTL_SET_OFB)
+  {
+    context->mode = AES_OFB;
+  }
+  else if (cmd == AESDEV_IOCTL_SET_CTR)
+  {
+    context->mode = AES_CTR;
+  }
+  else if (cmd == AESDEV_IOCTL_GET_STATE)
+  {
+    if (context->mode == AES_ECB_DECRYPT ||
+        context->mode == AES_ECB_ENCRYPT ||
+        context->mode == AES_UNDEF)
+      return -EINVAL;
+
+    printk(KERN_WARNING "Not supported yet!\n");
+    return -EINVAL;
+  }
+
+  if (copy_from_user(&context->key, (void *) arg, sizeof(aes128_block)))
+  {
+    printk(KERN_WARNING "copy_from_user\n");
+    return -EFAULT;
+  }
+
+  if (cmd != AESDEV_IOCTL_SET_ECB_ENCRYPT && cmd != AESDEV_IOCTL_SET_ECB_DECRYPT)
+  {
+    if (copy_from_user(&context->state, ((char *) arg) + sizeof(aes128_block), sizeof(aes128_block)))
+    {
+      printk(KERN_WARNING "copy_from_user\n");
+    }
+  }
+
   return 0;
 }
 /*****************************************************************************/
 
+/*** Device procedures *******************************************************/
+int init_cmd_buffer(aes128_dev *aes_dev)
+{
+  void __iomem *bar0;
+  char *k_buffer_ptr;
+  dma_addr_t d_buffer_ptr;
+
+  bar0 = aes_dev->bar0;
+
+  /* TODO: 16-byte alignment */
+  k_buffer_ptr = dma_alloc_coherent(&aes_dev->pci_dev->dev, AESDRV_CMDBUFF_SIZE,
+                                    &d_buffer_ptr, GFP_KERNEL);
+  aes_dev->cmd_buff_ptr = k_buffer_ptr;
+  aes_dev->d_cmd_buff_ptr = d_buffer_ptr;
+
+  iowrite32(d_buffer_ptr, bar0 + AESDEV_CMD_BEGIN_PTR);
+  /* TODO: Ostatni czy zaostatni? */
+  iowrite32(d_buffer_ptr + AESDRV_CMDBUFF_SIZE, bar0 + AESDEV_CMD_END_PTR);
+  iowrite32(d_buffer_ptr, bar0 + AESDEV_CMD_READ_PTR);
+  iowrite32(d_buffer_ptr, bar0 + AESDEV_CMD_WRITE_PTR);
+
+  return 0;
+}
+
+int init_aes_device(aes128_dev *aes_dev)
+{
+  void __iomem *bar0;
+
+  memset(aes_dev, 0, sizeof(aes128_dev));
+
+  mutex_init(&aes_dev->mutex);
+
+  bar0 = aes_dev->bar0;
+  SET_FLAG(bar0 + AESDEV_ENABLE,
+           AESDEV_ENABLE_XFER_DATA | AESDEV_ENABLE_FETCH_CMD);
+  SET_FLAG(bar0 + AESDEV_INTR_ENABLE, 0xFF);
+
+  return 0;
+}
+
+int destroy_aes_device(aes128_dev *aes_dev)
+{
+  mutex_destroy(&aes_dev->mutex);
+
+  return 0;
+}
+/*****************************************************************************/
 
 /*** PCI handlers ************************************************************/
-int aes_pci_probe(struct pci_dev *dev, const struct pci_device_id *id)
+int aes_pci_probe(struct pci_dev *pci_dev, const struct pci_device_id *id)
 {
   aes128_dev *aes_dev;
 
-  pci_enable_device(dev);
-  pci_request_regions(dev, "aesdev");
+  if (pci_enable_device(pci_dev))
+  {
+    printk(KERN_WARNING "pci_enable_device\n");
+    return -EFAULT;
+  }
+  if (pci_request_regions(pci_dev, "aesdev"))
+  {
+    printk(KERN_WARNING "pci_request_regions\n");
+    return -EFAULT;
+  }
   aes_dev = kmalloc(sizeof(aes128_dev), GFP_KERNEL);
-  aes_dev->bar0 = pci_iomap(dev, 0, 0);
-  pci_set_drvdata(dev, aes_dev);
+  aes_dev->bar0 = pci_iomap(pci_dev, 0, 0);
+  aes_dev->sys_dev = device_create(dev_class, NULL, MKDEV(major, dev_count), NULL, "aesdev%d", dev_count);
+  aes_dev->pci_dev = pci_dev;
+  pci_set_drvdata(pci_dev, aes_dev);
+
+  pci_set_master(pci_dev);
+  pci_set_dma_mask(pci_dev, DMA_BIT_MASK(32));
+  pci_set_consistent_dma_mask(pci_dev, DMA_BIT_MASK(32));
+
+
+  aes_devs[dev_count] = aes_dev;
+  dev_count++;
 
   printk(KERN_WARNING "Registered new aesdev\n");
 
@@ -109,21 +342,18 @@ void aes_pci_remove(struct pci_dev *dev)
 
 int aes_pci_resume(struct pci_dev *dev)
 {
-
+  return 0;
 }
 
 void aes_pci_shutdown(struct pci_dev *dev)
 {
-
 }
 
 int aes_pci_suspend(struct pci_dev *dev, pm_message_t state)
 {
-
+  return 0;
 }
 /*****************************************************************************/
-static struct device *dev;
-static struct class *dev_class;
 
 static int aesdev_init(void)
 {
@@ -132,10 +362,13 @@ static int aesdev_init(void)
 
   /* Rejestracja urządzenia w kernelu */
   dev_class = class_create(THIS_MODULE, "aesdev");
-  dev = device_create(dev_class, NULL, MKDEV(major, 0), NULL, "aesdev%d", 0);
 
   /* Rejestracja drivera PCI */
-  pci_register_driver(&aes_pci);
+  if (pci_register_driver(&aes_pci))
+  {
+    printk(KERN_WARNING "pci_register_driver\n");
+    return -EFAULT;
+  }
 
   /* Podpięcie pod vendor id i product id */
 //   return -EFAULT;
--- aesdev.h
+++ aesdev.h
@@ -5,6 +5,71 @@
 
 #include <linux/fs.h>
 #include <linux/pci.h>
+#include <linux/circ_buf.h>
+#include <linux/wait.h>
+
+#define AESDEV_VENDOR_ID    0x1af4
+#define AESDEV_DEVICE_ID    0x10fc
+
+#define AESDEV_ENABLE     0x000
+#define AESDEV_ENABLE_XFER_DATA   0x00000001
+#define AESDEV_ENABLE_FETCH_CMD   0x00000002
+#define AESDEV_STATUS     0x004
+#define AESDEV_STATUS_XFER_DATA   0x00000001
+#define AESDEV_STATUS_FETCH_CMD   0x00000002
+#define AESDEV_INTR     0x008
+#define AESDEV_INTR_ENABLE    0x00c
+
+#define AESDEV_AES_KEY(i)   (0x010 + (i))
+#define AESDEV_AES_DATA(i)    (0x020 + (i))
+#define AESDEV_AES_STATE(i)   (0x030 + (i))
+
+#define AESDEV_XFER_IN_PTR    0x040
+#define AESDEV_XFER_OUT_PTR   0x044
+#define AESDEV_XFER_STATE_PTR   0x048
+#define AESDEV_XFER_TASK    0x04c
+
+#define AESDEV_TASK(count, intr, save, mode)    ((count) << 12 | (intr) << 4 | (save) << 3 | (mode))
+#define AESDEV_TASK_MODE(task)          ((task) & 7)
+#define AESDEV_TASK_SAVE(task)          ((task) >> 3 & 1)
+#define AESDEV_TASK_INTR(task)          ((task) >> 4 & 0xff)
+#define AESDEV_TASK_COUNT(task)         ((task) >> 12 & 0xfffff)
+#define AESDEV_TASK_ACTIVE(task)        (!!((task) & ~0x7))
+
+#define AESDEV_MODE_ECB_ENCRYPT   0
+#define AESDEV_MODE_ECB_DECRYPT   1
+#define AESDEV_MODE_CBC_ENCRYPT   2
+#define AESDEV_MODE_CBC_DECRYPT   3
+#define AESDEV_MODE_CFB_ENCRYPT   4
+#define AESDEV_MODE_CFB_DECRYPT   5
+#define AESDEV_MODE_OFB     6
+#define AESDEV_MODE_CTR     7
+
+#define AESDEV_CMD_BEGIN_PTR    0x050
+#define AESDEV_CMD_END_PTR    0x054
+#define AESDEV_CMD_READ_PTR   0x058
+#define AESDEV_CMD_WRITE_PTR    0x05c
+
+#define AESDEV_AES_KEY_SIZE   0x10
+#define AESDEV_AES_BLOCK_SIZE   0x10
+
+#define AESDRV_IOBUFF_SIZE 0x1000
+#define AESDRV_CMDBUFF_SIZE 0x100
+
+#define SET_FLAG(reg, flag)\
+  do\
+  {\
+    unsigned int _current;\
+    _current = ioread32(reg);\
+    iowrite32(_current & ~(flag), reg);\
+  } while (0)
+#define RESET_FLAG(reg, flag)\
+  do\
+  {\
+    unsigned int _current;\
+    _current = ioread32(reg);\
+    iowrite32(_current | (flag), reg);\
+  } while (0)
 
 typedef enum
 {
@@ -21,12 +86,17 @@ typedef enum
 
 typedef struct
 {
-  uint8_t state[0x10];
+  uint8_t state[AESDEV_AES_BLOCK_SIZE];
 } aes128_block;
 
 typedef struct
 {
   void __iomem *bar0;
+  struct device *sys_dev;
+  struct pci_dev *pci_dev;
+  char *cmd_buff_ptr;
+  dma_addr_t d_cmd_buff_ptr;
+  struct mutex mutex;
 } aes128_dev;
 
 typedef struct
@@ -35,6 +105,9 @@ typedef struct
   aes128_block key;
   aes128_block state;
   aes128_dev *aes_dev;
+  struct circ_buf write_buffer;
+  struct circ_buf read_buffer;
+  struct mutex mutex;
 } aes128_context;
 
 /* Module handlers */
