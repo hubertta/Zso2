cb9116ed488f6e68b760e03505bfd7d8c8fc0336
hutabert <hutabert@gmail.com>
2015-05-18 00:14:22 +0200

    Rozbudowa testu 3

--- test3.c
+++ test3.c
@@ -18,7 +18,6 @@
 #include "aesdev_ioctl.h"
 
 int fd;
-FILE *f;
 const char *test_block = "1111111122222222"; // 31 31 31 31 31 31 31 31 32 32 32 32 32 32 32 32
 const char *test_key = "2222222244444444"; // 32 32 32 32 32 32 32 32 34 34 34 34 34 34 34 34
 const char *test_iv = "3333333355555555"; // 
@@ -263,22 +262,16 @@ test_cbc ()
   if (!is_equal (all_result, cipher1, 16)) ok = 0;
   assert_equal (all_result, cipher1, 16);
 
-  memcpy (key_iv + 16, iv2, 16);
-  set_mode (AESDEV_IOCTL_SET_CBC_ENCRYPT, key_iv);
   do_write (fd, text2, 16);
   do_read (fd, all_result, 16);
   if (!is_equal (all_result, cipher2, 16)) ok = 0;
   assert_equal (all_result, cipher2, 16);
 
-  memcpy (key_iv + 16, iv3, 16);
-  set_mode (AESDEV_IOCTL_SET_CBC_ENCRYPT, key_iv);
   do_write (fd, text3, 16);
   do_read (fd, all_result, 16);
   if (!is_equal (all_result, cipher3, 16)) ok = 0;
   assert_equal (all_result, cipher3, 16);
 
-  memcpy (key_iv + 16, iv4, 16);
-  set_mode (AESDEV_IOCTL_SET_CBC_ENCRYPT, key_iv);
   do_write (fd, text4, 16);
   do_read (fd, all_result, 16);
   if (!is_equal (all_result, cipher4, 16)) ok = 0;
@@ -291,6 +284,7 @@ test_cbc ()
   do_write (fd, text2, 16);
   do_write (fd, text1, 16);
   do_read (fd, all_result + 16, 48);
+  memcpy (key_iv + 16, iv4, 16);
   set_mode (AESDEV_IOCTL_SET_CBC_DECRYPT, key_iv);
   do_write (fd, all_result, 64);
   do_read (fd, all_result, 64);
@@ -307,6 +301,246 @@ test_cbc ()
   fprintf (stderr, "CBC enc/dec (2): %s\n", ok ? "ok" : "err");
 }
 
+void
+test_cfb ()
+{
+  const char *iv1 = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F";
+  const char *text1 = "\x6b\xc1\xbe\xe2\x2e\x40\x9f\x96\xe9\x3d\x7e\x11\x73\x93\x17\x2a";
+  const char *cipher1 = "\x3b\x3f\xd9\x2e\xb7\x2d\xad\x20\x33\x34\x49\xf8\xe8\x3c\xfb\x4a";
+
+  const char *iv2 = "\x3B\x3F\xD9\x2E\xB7\x2D\xAD\x20\x33\x34\x49\xF8\xE8\x3C\xFB\x4A";
+  const char *text2 = "\xae\x2d\x8a\x57\x1e\x03\xac\x9c\x9e\xb7\x6f\xac\x45\xaf\x8e\x51";
+  const char *cipher2 = "\xc8\xa6\x45\x37\xa0\xb3\xa9\x3f\xcd\xe3\xcd\xad\x9f\x1c\xe5\x8b";
+
+  const char *iv3 = "\xC8\xA6\x45\x37\xA0\xB3\xA9\x3F\xCD\xE3\xCD\xAD\x9F\x1C\xE5\x8B";
+  const char *text3 = "\x30\xc8\x1c\x46\xa3\x5c\xe4\x11\xe5\xfb\xc1\x19\x1a\x0a\x52\xef";
+  const char *cipher3 = "\x26\x75\x1f\x67\xa3\xcb\xb1\x40\xb1\x80\x8c\xf1\x87\xa4\xf4\xdf";
+
+  const char *iv4 = "\x26\x75\x1F\x67\xA3\xCB\xB1\x40\xB1\x80\x8C\xF1\x87\xA4\xF4\xDF";
+  const char *text4 = "\xf6\x9f\x24\x45\xdf\x4f\x9b\x17\xad\x2b\x41\x7b\xe6\x6c\x37\x10";
+  const char *cipher4 = "\xc0\x4b\x05\x35\x7c\x5d\x1c\x0e\xea\xc4\xc6\x6f\x9f\xf7\xf2\xe6";
+
+  const char *key = "\x2b\x7e\x15\x16\x28\xae\xd2\xa6\xab\xf7\x15\x88\x09\xcf\x4f\x3c";
+
+  char *all_text, *all_cipher, *all_result, *key_iv;
+
+  int ok;
+
+  all_text = malloc (16 * 4);
+  all_cipher = malloc (16 * 4);
+  all_result = malloc (16 * 4);
+  key_iv = malloc (16 * 2);
+
+  memcpy (key_iv, key, 16);
+  ok = 1;
+
+  /*** Test 1 ***/
+  memcpy (key_iv + 16, iv1, 16);
+  set_mode (AESDEV_IOCTL_SET_CFB_ENCRYPT, key_iv);
+  do_write (fd, text1, 16);
+  do_read (fd, all_result, 16);
+  if (!is_equal (all_result, cipher1, 16)) ok = 0;
+  assert_equal (all_result, cipher1, 16);
+
+  do_write (fd, text2, 16);
+  do_read (fd, all_result, 16);
+  if (!is_equal (all_result, cipher2, 16)) ok = 0;
+  assert_equal (all_result, cipher2, 16);
+
+  do_write (fd, text3, 16);
+  do_read (fd, all_result, 16);
+  if (!is_equal (all_result, cipher3, 16)) ok = 0;
+  assert_equal (all_result, cipher3, 16);
+
+  do_write (fd, text4, 16);
+  do_read (fd, all_result, 16);
+  if (!is_equal (all_result, cipher4, 16)) ok = 0;
+  assert_equal (all_result, cipher4, 16);
+
+  fprintf (stderr, "CFB encrypt (1): %s\n", ok ? "ok" : "err");
+
+  /*** Test 2 ***/
+  do_write (fd, text3, 16);
+  do_write (fd, text2, 16);
+  do_write (fd, text1, 16);
+  do_read (fd, all_result + 16, 48);
+  memcpy (key_iv + 16, iv4, 16);
+  set_mode (AESDEV_IOCTL_SET_CFB_DECRYPT, key_iv);
+  do_write (fd, all_result, 64);
+  do_read (fd, all_result, 64);
+  ok = 1;
+  if (!is_equal (all_result, text4, 16)) ok = 0;
+  if (!is_equal (all_result + 16, text3, 16)) ok = 0;
+  if (!is_equal (all_result + 32, text2, 16)) ok = 0;
+  if (!is_equal (all_result + 48, text1, 16)) ok = 0;
+  assert_equal (all_result, text4, 16);
+  assert_equal (all_result + 16, text3, 16);
+  assert_equal (all_result + 32, text2, 16);
+  assert_equal (all_result + 48, text1, 16);
+
+  fprintf (stderr, "CFB enc/dec (2): %s\n", ok ? "ok" : "err");
+}
+
+void
+test_ofb ()
+{
+  const char *iv1 = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F";
+  const char *text1 = "\x6b\xc1\xbe\xe2\x2e\x40\x9f\x96\xe9\x3d\x7e\x11\x73\x93\x17\x2a";
+  const char *cipher1 = "\x3b\x3f\xd9\x2e\xb7\x2d\xad\x20\x33\x34\x49\xf8\xe8\x3c\xfb\x4a";
+
+  const char *iv2 = "\x50\xFE\x67\xCC\x99\x6D\x32\xB6\xDA\x09\x37\xE9\x9B\xAF\xEC\x60";
+  const char *text2 = "\xae\x2d\x8a\x57\x1e\x03\xac\x9c\x9e\xb7\x6f\xac\x45\xaf\x8e\x51";
+  const char *cipher2 = "\x77\x89\x50\x8d\x16\x91\x8f\x03\xf5\x3c\x52\xda\xc5\x4e\xd8\x25";
+
+  const char *iv3 = "\xD9\xA4\xDA\xDA\x08\x92\x23\x9F\x6B\x8B\x3D\x76\x80\xE1\x56\x74";
+  const char *text3 = "\x30\xc8\x1c\x46\xa3\x5c\xe4\x11\xe5\xfb\xc1\x19\x1a\x0a\x52\xef";
+  const char *cipher3 = "\x97\x40\x05\x1e\x9c\x5f\xec\xf6\x43\x44\xf7\xa8\x22\x60\xed\xcc";
+
+  const char *iv4 = "\xA7\x88\x19\x58\x3F\x03\x08\xE7\xA6\xBF\x36\xB1\x38\x6A\xBF\x23";
+  const char *text4 = "\xf6\x9f\x24\x45\xdf\x4f\x9b\x17\xad\x2b\x41\x7b\xe6\x6c\x37\x10";
+  const char *cipher4 = "\x30\x4c\x65\x28\xf6\x59\xc7\x78\x66\xa5\x10\xd9\xc1\xd6\xae\x5e";
+
+  const char *key = "\x2b\x7e\x15\x16\x28\xae\xd2\xa6\xab\xf7\x15\x88\x09\xcf\x4f\x3c";
+
+  char *all_text, *all_cipher, *all_result, *key_iv;
+
+  int ok;
+
+  all_text = malloc (16 * 4);
+  all_cipher = malloc (16 * 4);
+  all_result = malloc (16 * 4);
+  key_iv = malloc (16 * 2);
+
+  memcpy (key_iv, key, 16);
+  ok = 1;
+
+  /*** Test 1 ***/
+  memcpy (key_iv + 16, iv1, 16);
+  set_mode (AESDEV_IOCTL_SET_OFB, key_iv);
+  do_write (fd, text1, 16);
+  do_read (fd, all_result, 16);
+  if (!is_equal (all_result, cipher1, 16)) ok = 0;
+  assert_equal (all_result, cipher1, 16);
+
+  do_write (fd, text2, 16);
+  do_read (fd, all_result, 16);
+  if (!is_equal (all_result, cipher2, 16)) ok = 0;
+  assert_equal (all_result, cipher2, 16);
+
+  do_write (fd, text3, 16);
+  do_read (fd, all_result, 16);
+  if (!is_equal (all_result, cipher3, 16)) ok = 0;
+  assert_equal (all_result, cipher3, 16);
+
+  do_write (fd, text4, 16);
+  do_read (fd, all_result, 16);
+  if (!is_equal (all_result, cipher4, 16)) ok = 0;
+  assert_equal (all_result, cipher4, 16);
+
+  fprintf (stderr, "OFB encrypt (1): %s\n", ok ? "ok" : "err");
+
+  /*** Test 2 ***/
+  do_write (fd, text3, 16);
+  do_write (fd, text2, 16);
+  do_write (fd, text1, 16);
+  do_read (fd, all_result + 16, 48);
+  memcpy (key_iv + 16, iv4, 16);
+  set_mode (AESDEV_IOCTL_SET_OFB, key_iv);
+  do_write (fd, all_result, 64);
+  do_read (fd, all_result, 64);
+  ok = 1;
+  if (!is_equal (all_result, text4, 16)) ok = 0;
+  if (!is_equal (all_result + 16, text3, 16)) ok = 0;
+  if (!is_equal (all_result + 32, text2, 16)) ok = 0;
+  if (!is_equal (all_result + 48, text1, 16)) ok = 0;
+  assert_equal (all_result, text4, 16);
+  assert_equal (all_result + 16, text3, 16);
+  assert_equal (all_result + 32, text2, 16);
+  assert_equal (all_result + 48, text1, 16);
+
+  fprintf (stderr, "OFB enc/dec (2): %s\n", ok ? "ok" : "err");
+}
+
+void
+test_ctr ()
+{
+  const char *iv = "\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff";
+  const char *text1 = "\x6b\xc1\xbe\xe2\x2e\x40\x9f\x96\xe9\x3d\x7e\x11\x73\x93\x17\x2a";
+  const char *cipher1 = "\x87\x4d\x61\x91\xb6\x20\xe3\x26\x1b\xef\x68\x64\x99\x0d\xb6\xce";
+
+  const char *text2 = "\xae\x2d\x8a\x57\x1e\x03\xac\x9c\x9e\xb7\x6f\xac\x45\xaf\x8e\x51";
+  const char *cipher2 = "\x98\x06\xf6\x6b\x79\x70\xfd\xff\x86\x17\x18\x7b\xb9\xff\xfd\xff";
+
+  const char *text3 = "\x30\xc8\x1c\x46\xa3\x5c\xe4\x11\xe5\xfb\xc1\x19\x1a\x0a\x52\xef";
+  const char *cipher3 = "\x5a\xe4\xdf\x3e\xdb\xd5\xd3\x5e\x5b\x4f\x09\x02\x0d\xb0\x3e\xab";
+
+  const char *text4 = "\xf6\x9f\x24\x45\xdf\x4f\x9b\x17\xad\x2b\x41\x7b\xe6\x6c\x37\x10";
+  const char *cipher4 = "\x1e\x03\x1d\xda\x2f\xbe\x03\xd1\x79\x21\x70\xa0\xf3\x00\x9c\xee";
+
+  const char *key = "\x2b\x7e\x15\x16\x28\xae\xd2\xa6\xab\xf7\x15\x88\x09\xcf\x4f\x3c";
+
+  char *all_text, *all_cipher, *all_result, *key_iv;
+  char *iv2;
+
+  int ok;
+
+  all_text = malloc (16 * 4);
+  all_cipher = malloc (16 * 4);
+  all_result = malloc (16 * 4);
+  key_iv = malloc (16 * 2);
+  iv2 = malloc (16);
+
+  memcpy (key_iv, key, 16);
+  ok = 1;
+
+  /*** Test 1 ***/
+  memcpy (key_iv + 16, iv, 16);
+  set_mode (AESDEV_IOCTL_SET_CTR, key_iv);
+  do_write (fd, text1, 16);
+  do_read (fd, all_result, 16);
+  if (!is_equal (all_result, cipher1, 16)) ok = 0;
+  assert_equal (all_result, cipher1, 16);
+
+  do_write (fd, text2, 16);
+  do_read (fd, all_result, 16);
+  if (!is_equal (all_result, cipher2, 16)) ok = 0;
+  assert_equal (all_result, cipher2, 16);
+
+  do_write (fd, text3, 16);
+  do_read (fd, all_result, 16);
+  if (!is_equal (all_result, cipher3, 16)) ok = 0;
+  assert_equal (all_result, cipher3, 16);
+  
+  set_mode (AESDEV_IOCTL_GET_STATE, key_iv + 16);
+
+  do_write (fd, text4, 16);
+  do_read (fd, all_result, 16);
+  if (!is_equal (all_result, cipher4, 16)) ok = 0;
+  assert_equal (all_result, cipher4, 16);
+
+  fprintf (stderr, "CTR encrypt (1): %s\n", ok ? "ok" : "err");
+
+  /*** Test 2 ***/
+  do_write (fd, text3, 16);
+  do_write (fd, text2, 16);
+  do_write (fd, text1, 16);
+  do_read (fd, all_result + 16, 48);
+  set_mode (AESDEV_IOCTL_SET_CTR, key_iv);
+  do_write (fd, all_result, 64);
+  do_read (fd, all_result, 64);
+  ok = 1;
+  if (!is_equal (all_result, text4, 16)) ok = 0;
+  if (!is_equal (all_result + 16, text3, 16)) ok = 0;
+  if (!is_equal (all_result + 32, text2, 16)) ok = 0;
+  if (!is_equal (all_result + 48, text1, 16)) ok = 0;
+  assert_equal (all_result, text4, 16);
+  assert_equal (all_result + 16, text3, 16);
+  assert_equal (all_result + 32, text2, 16);
+  assert_equal (all_result + 48, text1, 16);
+
+  fprintf (stderr, "CTR enc/dec (2): %s\n", ok ? "ok" : "err");
+}
+
 /*****************************************************************************/
 
 int
@@ -316,6 +550,9 @@ main ()
 
   test_ecb ();
   test_cbc ();
+  test_cfb ();
+  test_ofb ();
+  test_ctr ();
 
   close (fd);
 
